8cf81265bd2bd486ff163cc69c17e56841ea0188
Tony Hong <tonyhong63@gmail.com>
2018-08-22 17:09:10 -0400

    Made code more readable

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ import './index.css';
 
 function Square (props) { 
     return (
-      <button className="square" 
-      onClick={props.onClick}>{props.value}
+      <button className = "square" 
+      onClick = {props.onClick} > {props.value}
       </button>
     );
 }
 
-function calculateWinner(squares) {
+function calculateWinner (squares) {
   const lines = [
     [0, 1, 2],
     [3, 4, 5],
@@ -31,9 +31,9 @@ function calculateWinner(squares) {
   return null;
 }
 
-function isTie(squares){
+function isTie (squares) {
   for (let i in squares){
-    if(squares[i]===null){
+    if(squares[i] === null){
       return false;
     }
   }
@@ -43,67 +43,67 @@ function isTie(squares){
 
 
 class Board extends React.Component {
-  defaultstate={
-    squares:Array(9).fill(null),
-    xIsNext:true,
+  defaultstate = {
+    squares : Array(9).fill(null),
+    xIsNext : true,
   }
 
-  constructor(props){
-    super(props);
-    this.state=this.defaultstate;
+  constructor (props) {
+    super (props);
+    this.state = this.defaultstate;
   }
-  renderSquare(i) {
-    return <Square value={
+  renderSquare (i) {
+    return <Square value = {
       this.state.squares[i]}
-      onClick={()=>this.handleClick(i)} />;
+      onClick = {() => this.handleClick(i)} />;
   }
-  resetGame(){
+  resetGame () {
     this.setState({...this.defaultstate});
   }
-  handleClick(i){
-    const squares=this.state.squares.slice();
-    if(squares[i]||calculateWinner(this.state.squares)){
+  handleClick (i) {
+    const squares = this.state.squares.slice();
+    if(squares[i] || calculateWinner (this.state.squares)) {
       return;
     }
-    squares[i]=this.state.xIsNext?'X':'O';
-    this.setState({
-      squares:squares,
-      xIsNext:!this.state.xIsNext,
+    squares[i] = this.state.xIsNext ? 'X':'O';
+    this.setState ({
+      squares : squares,
+      xIsNext : !this.state.xIsNext,
       });
 
   }
 
-  render() {
-    const winner=calculateWinner(this.state.squares);
+  render () {
+    const winner = calculateWinner(this.state.squares);
     let status;
-    if(!winner&&!isTie(this.state.squares)){
-      status = 'Next player: '+ (this.state.xIsNext ? 'X':'O');
+    if(!winner && !isTie (this.state.squares)) {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X':'O');
     }
-    else if(winner){
-      status='Winner: '+winner;
+    else if (winner) {
+      status = 'Winner: '+ winner;
     }
     else{
-      status='Tie';
+      status = 'Tie';
     }
     return (
       <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
+        <div className = "status">{status}</div>
+        <div className = "board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
           {this.renderSquare(2)}
         </div>
-        <div className="board-row">
+        <div className = "board-row">
           {this.renderSquare(3)}
           {this.renderSquare(4)}
           {this.renderSquare(5)}
         </div>
-        <div className="board-row">
+        <div className = "board-row">
           {this.renderSquare(6)}
           {this.renderSquare(7)}
           {this.renderSquare(8)}
         </div>
-        <button onClick={()=>this.resetGame()}>Reset Game</button>
+        <button onClick = {() => this.resetGame()}>Reset Game</button>
       </div>
     );
   }
@@ -112,11 +112,11 @@ class Board extends React.Component {
 class Game extends React.Component {
   render() {
     return (
-      <div className="game">
-        <div className="game-board">
+      <div className = "game">
+        <div className = "game-board">
           <Board />
         </div>
-        <div className="game-info">
+        <div className = "game-info">
           <div>{/* status */}</div>
           <ol>{/* TODO */}</ol>
         </div>
